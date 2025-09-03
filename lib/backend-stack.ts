2afed9bcd64bf6be1b863ad565daf53d90ab97c6
Ahmed Utku ÖZÜDOĞRU <ahmedutkuozudogru@gmail.com>
2025-09-03 21:27:45 +0300

    Storage Stack Completed

--- lib/backend-stack.ts
+++ lib/backend-stack.ts
@@ -1,16 +1,33 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as kms from "aws-cdk-lib/aws-kms";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as ddb from "aws-cdk-lib/aws-dynamodb";
+import { Construct } from "constructs";
+import {
+  CognitoUserPoolsAuthorizer,
+  LambdaIntegration,
+  RestApi,
+  MethodOptions,
+  AuthorizationType,
+} from "aws-cdk-lib/aws-apigateway";
+import { UserPool, CfnUserPoolGroup } from "aws-cdk-lib/aws-cognito";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { join } from "path";
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'BackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // outputs:
+    new cdk.CfnOutput(this, "UserPoolId", {
+      value: userPool.userPoolId,
+    });
+    new cdk.CfnOutput(this, "UserPoolClientId", {
+      value: userPoolClient.userPoolClientId,
+    });
+    new cdk.CfnOutput(this, "ApiUrl", {
+      value: api.url,
+    });
   }
 }
