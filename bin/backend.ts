ea0c9807965eba7651f1ca8aedf3561bd873a6bd
Berkay <146650666+berkaydemircin@users.noreply.github.com>
2025-09-28 20:22:34 +0300

    Deleted old/outdated files

--- bin/backend.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from "aws-cdk-lib";
-//import { BackendStack } from "../lib/backend-stack";
-import { storageStackConfig } from "./config";
-import { StorageStack } from "../lib/storage-stack";
-
-const app = new cdk.App();
-new StorageStack(app, "StorageStack", storageStackConfig);
